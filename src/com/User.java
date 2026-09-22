dd30203fc435c6b044c3affc1148416c30a18ada
Vishnu bharathi K <vishnubharathik@RGML-LAP-163.local>
2026-09-22 15:35:51 +0530

    Add constructors and setters to User

--- src/com/User.java
+++ src/com/User.java
@@ -1,16 +1,31 @@
 package com;
 
 public class User {
-	private int userId;
-	private String name;
-	
-	public int getUserId() {
-		return userId;
-	}
-	public String getName() {
-		return name;
-	}
-	
-	
-	
+
+    private int userId;
+    private String name;
+
+    public User() {
+    }
+
+    public User(int userId, String name) {
+        this.userId = userId;
+        this.name = name;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
